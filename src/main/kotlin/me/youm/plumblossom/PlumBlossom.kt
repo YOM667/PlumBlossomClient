c70b2758191584f994b1bec3174bf341bdd6020d
You_M <1055965862@qq.com>
2023-01-12 00:26:16 +0800

    update project name and mixin file name

--- src/main/kotlin/me/youm/plumblossom/PlumBlossom.kt
+++ src/main/kotlin/me/youm/plumblossom/PlumBlossom.kt
@@ -1,10 +1,16 @@
 package me.youm.plumblossom
 
 import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 class PlumBlossom : ModInitializer {
-
+    companion object{
+        const val NAME = "PlumBlossom"
+        val AUTHORS = arrayOf("YouM");
+        val log :Logger = LogManager.getLogger()
+    }
     override fun onInitialize() {
-        //TODO - "Not yet implemented"
+        log.info("Hello Kotlin Fabric MOD")
     }
 }
